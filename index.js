05c9f52c501da10b799cef10907792e910c8933a
HI Po <prantokih42@gmail.com>
2024-06-03 01:48:27 +0600

    meals collection add

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const mealsCollection = client.db("Hostel").collection("meals");
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
