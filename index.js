1a32a51ecef4e8ff3c6e046aa23722e99f4a1aa2
HI Po <prantokih42@gmail.com>
2024-06-07 04:27:31 +0600

    profile added

--- index.js
+++ index.js
@@ -2,20 +2,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.port || 5000;
-
-
-
+const port = process.env.PORT || 5000;
+const { MongoClient, ObjectId, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.texsw4y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,128 +19,78 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-
-
     const mealsCollection = client.db("Hostel").collection("meals");
     const reviewCollection = client.db("Hostel").collection("reviews");
+    const userCollection = client.db("Hostel").collection("users");
 
     app.get('/meals', async (req, res) => {
       const result = await mealsCollection.find().toArray();
       res.send(result);
-    })
-    app.get('/reviews', async (req, res) => {
-      const result = await reviewCollection.find().toArray();
+    });
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
       res.send(result);
-    })
-    app.get('/meals', async (req, res) => {
-      const cursor = mealsCollection.find().sort({ "averageCost": -1 });
-      const result = await cursor.toArray();
+    });
+
+    app.get('/meals/:id', async (req, res) => {
+      const id = req.params.id; 
+      const query = { _id: new ObjectId(id) };
+      const result = await mealsCollection.findOne(query);
       res.send(result);
-    })
+    });
+
     app.post('/meals', async (req, res) => {
       const addFood = req.body;
-      console.log(addFood);
       const result = await mealsCollection.insertOne(addFood);
       res.send(result);
+    });
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    
 
-    })
-    app.patch('/restaurant/:id', async (req, res) => {
+    app.patch('/meals/uptodate/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedFood = req.body;
       const food = {
         $set: {
           title: updatedFood.title,
-          quantity: updatedFood.quantity,
-          borrowedFoods: updatedFood.borrowedFoods,
+          like: updatedFood.like,
           description: updatedFood.description,
           price: updatedFood.price,
-          Category: updatedFood.category,
+          category: updatedFood.category,
           adminName: updatedFood.adminName,
-          Image: updatedFood.Image,
-          isSold: true,
-          buyersEmail: updatedFood.buyersEmail
-        }
-      }
-      const result = await mealsCollection.updateOne(filter, food)
-      res.send(result)
-
-    })
-    app.patch('/restaurant/uptodate/:id', async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updatedFood = req.body;
-      const food = {
-        $set: {
-       title: updatedFood.title,
-          quantity: updatedFood.quantity,
-          borrowedFoods: updatedFood.borrowedFoods,
-          description: updatedFood.description,
-          price: updatedFood.price,
-          Category: updatedFood.category,
-          adminName: updatedFood.adminName,
-          Image: updatedFood.Image,
-          isSold: true,
+          mealImage: updatedFood.mealImage,
+          isSold: updatedFood.isSold,
           buyersEmail: updatedFood.buyersEmail,
           time: updatedFood.time
         }
-      }
-      const result = await mealsCollection.updateOne(filter, food)
-      res.send(result)
-    })
-
-
-    app.get("/restaurant/uptodate/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await mealsCollection.findOne(query);
+      };
+      const result = await mealsCollection.updateOne(filter, food);
       res.send(result);
     });
-    app.get("/restaurant/:email", async (req, res) => {
-      console.log(req.params.email);
-      const result = await mealsCollection.find({ email: req.params.email }).toArray();
-      res.send(result)
-    })
-    app.get('/restaurant/update/:id', async (req, res) => {
-      const cursor = mealsCollection.find();
-      const result = await cursor.toArray();
+
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
       res.send(result);
-    })
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-
   }
 }
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-  res.send('Hostel is processing ')
-})
+  res.send('Hostel is processing');
+});
 
 app.listen(port, () => {
   console.log(`Hostel is running on ${port}`);
-})
+});
+  
