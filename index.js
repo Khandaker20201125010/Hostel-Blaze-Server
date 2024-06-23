20a3195d7073440b23cc919ee0d1007888e77cb8
HI Po <prantokih42@gmail.com>
2024-06-23 23:39:14 +0600

    membershiip added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 const { MongoClient, ObjectId, ServerApiVersion } = require('mongodb');
 
@@ -25,6 +26,8 @@ async function run() {
     const upComingMealsCollection = client.db("Hostel").collection("upComingMeals");
     const userCollection = client.db("Hostel").collection("users");
     const cartCollection = client.db("Hostel").collection("carts");
+    const membershipCollection = client.db("Hostel").collection("membership");
+    const paymentCollection = client.db("Hostel").collection("paymentCollection");
     app.post('/jwt', async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' });
@@ -65,7 +68,12 @@ async function run() {
       const result = await mealsCollection.find().toArray();
       res.send(result);
     });
-   
+
+    app.get('/membership', async (req, res) => {
+      const result = await membershipCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post('/meals', verifyToken, verifyAdmin, async (req, res) => {
       const item = req.body;
       const result = await mealsCollection.insertOne(item);
@@ -84,12 +92,54 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
+    app.get('/users/id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
     app.get('/carts', async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
+    app.get('/membership', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await mealsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.patch('/users/membership', async (req, res) => {
+      const { email, badge, transactionId, date, price } = req.body;
+  
+      try {
+          const filter = { email: email };
+          const updateDoc = {
+              $set: {
+                  badge: badge,
+                  transactionId: transactionId,
+                  transactionDate: date,
+                  price: price
+              }
+          };
+  
+          const result = await userCollection.updateOne(filter, updateDoc);
+          console.log('Update Result:', result);
+  
+          if (result.modifiedCount > 0) {
+              res.status(200).json({ message: 'Membership updated successfully' });
+          } else {
+              res.status(400).json({ message: 'Failed to update membership' });
+          }
+      } catch (error) {
+          console.error('Internal Server Error', error);
+          res.status(500).json({ message: 'Internal Server Error', error });
+      }
+  });
+  
+
     app.post('/meals', async (req, res) => {
       const item = req.body;
       const result = await mealsCollection.insertOne(item);
@@ -117,24 +167,11 @@ async function run() {
       const item = req.body;
       // Ensure reviews are stored as an array in MongoDB
       if (!Array.isArray(item.reviews)) {
-          item.reviews = [];
+        item.reviews = [];
       }
       const result = await mealsCollection.insertOne(item);
       res.send(result);
-  });
-  app.post('/users', async (req, res) => {
-    const { email } = req.body;
-    const filter = { email: email };
-    const updateDoc = {
-        $set: {
-            isSubscribed: true,
-        },
-    };
-    const result = await userCollection.updateOne(filter, updateDoc);
-    res.send({ success: result.modifiedCount > 0 });
-});
-
-
+    });
 
 
 
@@ -144,15 +181,51 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result)
     })
+    ///payment method
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, 'amount inside the intent')
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
+
+
+    app.patch('/users/badge/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+        const badge = req.body;
+        console.log(badge)
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: { badge: badge }
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } catch (error) {
+        console.error('Error updating user badge:', error);
+        res.status(500).send({ error: 'Failed to update user badge' });
+      }
+    });
+
     app.post('/users', async (req, res) => {
       const user = req.body;
-      const query = { email: user?.email }
-      const existingUser = await userCollection.findOne(query)
+
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
       if (existingUser) {
-        return
+        return res.send({ message: 'user already exists', insertedId: null })
       }
-      const result = await userCollection.insertOne(user)
-      res.send(result)
+      const result = await userCollection.insertOne(user);
+      res.send(result);
     });
     app.patch('/menu/:id', async (req, res) => {
       const item = req.body;
@@ -170,19 +243,8 @@ async function run() {
       const result = await mealsCollection.updateOne(filter, updatedDoc)
       res.send(result);
     })
-    app.post('/users', async (req, res) => {
-      const { email } = req.body;
-      const filter = { email: email };
-      const updateDoc = {
-          $set: {
-              isSubscribed: true,
-          },
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send({ success: result.modifiedCount > 0 });
-  });
-  
-  
+
+
 
     app.get('/users/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
@@ -205,7 +267,7 @@ async function run() {
     //   }
     //   res.send({ admin });
     // })
-    
+
     app.get('/users/admin/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
 
@@ -233,6 +295,21 @@ async function run() {
       const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     })
+    app.patch('/users/sub/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: { subscription: 'subscribed' } // Assuming 'subscription' is the field name in your user schema
+      };
+
+      try {
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: 'Failed to update subscription', error });
+      }
+    });
+
 
     app.patch('/meals/like/:id', async (req, res) => {
       const id = req.params.id;
@@ -248,12 +325,12 @@ async function run() {
           adminName: updatedFood.adminName,
           mealImage: updatedFood.mealImage,
           email: updatedFood.email,
-          rating:updatedFood.rating,
-          ingredients:updatedFood.ingredients,
+          rating: updatedFood.rating,
+          ingredients: updatedFood.ingredients,
           postTime: updatedFood.postTime,
-          reviews:updatedFood.reviews,
-          distributorName:updatedFood.distributorName,
-          likers:updatedFood.likers
+          reviews: updatedFood.reviews,
+          distributorName: updatedFood.distributorName,
+          likers: updatedFood.likers
         }
       };
       const result = await mealsCollection.updateOne(filter, likes);
@@ -264,7 +341,7 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await mealsCollection.deleteOne(query);
       res.send(result);
-    
+
     });
     app.patch('/meals/reviews/:id', async (req, res) => {
       const id = req.params.id;
@@ -280,12 +357,12 @@ async function run() {
           adminName: updatedFood.adminName,
           mealImage: updatedFood.mealImage,
           email: updatedFood.email,
-          rating:updatedFood.rating,
-          ingredients:updatedFood.ingredients,
+          rating: updatedFood.rating,
+          ingredients: updatedFood.ingredients,
           postTime: updatedFood.postTime,
-          reviews:updatedFood.reviews,
-          distributorName:updatedFood.distributorName,
-          likers:updatedFood.likers
+          reviews: updatedFood.reviews,
+          distributorName: updatedFood.distributorName,
+          likers: updatedFood.likers
         }
       };
       const result = await mealsCollection.updateOne(filter, likes);
@@ -296,36 +373,57 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await mealsCollection.deleteOne(query);
       res.send(result);
-    
+
     });
+
     // app.patch('/meals/reviews/:email', async (req, res) => {
     //   const email = req.params.email;
     //   const query = { email:email };
     //   const result = await mealsCollection.updateOne(query,{$set:(reviews:[])});
     //   res.send(result);
-    
+
     // });
+    //subs
+
     app.patch('/meals/reviews/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const { reviews } = req.body; // The updated reviews array
-  
+
       const updateDoc = {
-          $set: {
-              reviews: reviews
-          }
+        $set: {
+          reviews: reviews
+        }
       };
-  
+
       try {
-          const result = await mealsCollection.updateOne(filter, updateDoc);
-          res.send(result);
+        const result = await mealsCollection.updateOne(filter, updateDoc);
+        res.send(result);
       } catch (error) {
-          res.status(500).send({ message: 'Failed to update reviews', error });
+        res.status(500).send({ message: 'Failed to update reviews', error });
       }
-  });
-  
-    
-    
+    });
+    //dta
+    app.patch('/meals/reviews/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const { reviews } = req.body;
+
+      const updateDoc = {
+        $set: { reviews: reviews }
+      };
+
+      try {
+        const result = await mealsCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: 'Failed to update reviews', error });
+      }
+    });
+
+
+
+
     app.patch('/meals/uptodate/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
